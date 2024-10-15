ba35d1baaada042a8396707cb9c8a16b7f74eefa
lorenzoggggg <132879804+lorenzoggggg@users.noreply.github.com>
2024-10-15 13:22:13 +1300

    Add files via upload

---
+++ indexjs.js
@@ -0,0 +1,9 @@
+function yahooClick() {
+    var yahoo = document.getElementById("yahoo");
+    yahoo.play();
+}
+
+function meowClick() {
+    var meow = document.getElementById("meow");
+    meow.play();
+}

f73c5aadf46de2399b5c89e298dbd3e561853d5b
lorenzoggggg <132879804+lorenzoggggg@users.noreply.github.com>
2024-10-15 13:21:59 +1300

    Delete indexjs.js

--- indexjs.js
+++
@@ -1,9 +0,0 @@
-function yahooClick() {
-    var yahoo = document.getElementById("yahoo");
-    yahoo.play();
-}
-
-function meowClick() {
-    var meow = document.getElementById("meow");
-    meow.play();
-}
